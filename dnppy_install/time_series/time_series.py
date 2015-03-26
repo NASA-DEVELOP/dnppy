d5f5aca33e0ad95952c42a102e59171789d51892
Jeff <jeff.ely.08@gmail.com>
2015-03-26 17:54:31 -0400

    time_series __getitem__ works now!

--- dnppy_install/time_series/time_series.py
+++ dnppy_install/time_series/time_series.py
@@ -27,7 +27,7 @@ class time_series:
     time series, where the steps of the method depend on weather the time
     series is at its smallest subset or not.
 
-    (it isn't quite finished yet) 
+    (it isn't quite finished yet)
 
     MEMORY WARNING. The entirety of the dataset is represented in every layer
     of discretization, so watch out for exploding memory consumption by
@@ -40,7 +40,7 @@ class time_series:
 
     this should allow far more elegant replacement of the follwing funtions
     within the dnppy module:
-    
+
     "files_in_window"
     "rolling_window"
     "rolling_stats"
@@ -48,7 +48,7 @@ class time_series:
     """
 
     def __init__(self, name = "Nameless", units = False, discretized = False, disc_level = 0):
-        """initializes the time series"""
+        """ initializes the time series """
 
         self.name           = name          # the name of this time series
         self.units          = units         # unit of time represented by subset if discretized
@@ -56,17 +56,17 @@ class time_series:
         self.disc_level     = disc_level    # the subset level of this time_series
 
         self.rasterpaths    = False         # becomes true if time series is of raster data
-        
+
         self.fmt            = False         # for interpreting timestrings to time objs
         self.headers        = []            # one header for each col in dataset
-        
+
         self.time_col       = 0             # index of data column with time info
         self.time           = []            # separate copy of data[time_col]
         self.time_dom       = False         # self.time converted to list of datetime objs
         self.time_dec_days  = []            # self.time converted to mono rising decimal days
-        
+
         self.subsets        = []            # object list containing constituent time_seires
-        
+
         self.row_data       = []            # row wise dataset
         self.col_data       = []            # column wise dataset, built as dict
 
@@ -74,36 +74,74 @@ class time_series:
         return
 
 
-    def __getitem___(self, index):
-        if self.discretized:
-            return self.subsets[index].row_data
+    def __getitem__(self, arg):
+        """
+        allows subsets of the time series to be accessed as follows
+
+        Time series whos first row is:
+        ['20010101', '045316', '1', '20010101045316', 366]
+
+        With no discretezation layers
+        comand        : result
+        ts[0]         : ['20010101', '045316', '1', '20010101045316', 366]
+        ts[0][0]      : 20010101
+
+        with one discretezation layer
+        ts[0]         : <__main__.time_series instance at 0x00000000023CEB48>
+        ts[0][0]      : ['20010101', '045316', '1', '20010101045316', 366]
+        ts[0][0][0]   : 20010101
+
+        with two discretezation layers
+        ts[0]         : <__main__.time_series instance at 0x00000000023CEB48>
+        ts[0][0]      : <__main__.time_series instance at 0x00000000023CCA48>
+        ts[0][0][0]   : ['20010101', '045316', '1', '20010101045316', 366]
+        ts[0][0][0][0]: 20010101
+        """
+
+        if isinstance(arg, slice):
+            a = arg.start
+            b = arg.stop
+
+            if self.discretized:
+                if b > len(self.subsets): b = len(self.subsets)
+                return [self.subsets[x] for x in range(a,b)]
+
+            else:
+                if b > len(self.row_data): b = len(self.row_data)
+                return [self.row_data[x] for x in range(a,b)]
+
         else:
-            return self.row_data[index]
-        
-    
+            if self.discretized:
+                return self.subsets[arg]
+
+            else:
+                return list(self.row_data[arg])
+
+
+
     def _get_atts_from(self, parent_time_series):
         """
         Allows bulk setting of attributes. usefull for allowing a subset to inherit
         information from its parent time_series
-        
+
         internal use only
         """
-        
+
         self.fmt            = parent_time_series.fmt
         self.headers        = parent_time_series.headers
         self.time_col       = parent_time_series.time_col
         self.time_header    = parent_time_series.time_header
         self.disc_level     = parent_time_series.disc_level + 1
         return
-    
-    
+
+
     def from_csv(self, csv_path):
         """ creates the time_series data from a csv"""
 
         if not self.discretized:
             self.row_data, self.headers = csv_io.read_csv_rows(csv_path)
             self.build_col_data()
-            
+
         else:
             raise Warning("You may not import new data into \
                           an already discretized time series")
@@ -112,10 +150,10 @@ class time_series:
 
     def to_csv(self, csv_path):
         """ simply write the data to a csv file"""
-        
+
         print("Saved time series '{0}' with {1} rows and {2} columns".format(
                                     self.name, len(self.row_data), len(self.col_data)))
-              
+
         csv_io.write_csv_rows(self.row_data, self.headers, csv_path)
         return
 
@@ -137,7 +175,7 @@ class time_series:
 
         self.headers = ['filepaths','filenames']
         self.row_data = []
-        
+
         for filepath in filepaths:
             head, filename = os.path.split(filepath)
             self.row_data.append([filepath, filename])
@@ -147,12 +185,12 @@ class time_series:
 
         # flags this time series as being comprised of rasterpaths
         self.rasterpaths  = True
-        
+
         return
-                
+
 
     def build_col_data(self):
-        """ builds columnwise data matrix with an actual dict"""
+        """ builds columnwise data matrix with an actual dict """
 
         temp_col = zip(*self.row_data)
 
@@ -160,14 +198,14 @@ class time_series:
         for i,col in enumerate(temp_col):
             self.col_data[self.headers[i]] = col
         return
-    
+
 
     def clean(self, col_header):
         """ Removes rows where the specified column has an invalid number"""
 
         if not col_header in self.headers:
-            raise LookupError("{0} header not in dataset!".format(col_header)) 
-            
+            raise LookupError("{0} header not in dataset!".format(col_header))
+
         col_index = self.headers.index(col_header)
         temp_data = self.row_data
         self.row_data = []
@@ -179,9 +217,9 @@ class time_series:
             except:
                 self.bad_rows.append(row)
                 continue
-            
+
             self.row_data.append(row)
-                
+
         print("Removed {0} rows with invalid '{1}'".format(len(self.bad_rows),col_header))
         print("Dataset now has {0} rows".format(len(self.row_data)))
 
@@ -189,12 +227,12 @@ class time_series:
             for subset in self.subsets:
                 subset.clean(col_header)
         return
-    
-                
+
+
     def add_row(self, row):
         """ simply adds a row to the time_series data"""
-        
-        self.row_data.append(row)
+
+        self.row_data.append(list(row))
         return
 
 
@@ -214,11 +252,11 @@ class time_series:
                         self.row_data.append(row)
 
                 self.define_time(self.time_header, self.fmt)
-        
+
         if destroy_subsets:
             self.subsets = []
             self.discretized = False
-            
+
         return
 
 
@@ -231,17 +269,17 @@ class time_series:
 
         self.time_header = time_header
         self.build_col_data()
-        
+
         if time_header in self.headers:
                 self.time_col   = self.headers.index(time_header)
                 self.time       = self.col_data[time_header]
         else:
             raise LookupError("Time header not in dataset!")
-        
+
         if self.discretized:
             for subset in self.subsets:
                 subset._extract_time(time_header)
-                
+
         return self.time
 
 
@@ -249,10 +287,10 @@ class time_series:
         """merges two columns together (string concatenation)"""
 
         new_header  = "_".join([header1, header2])
-        
+
         for i, entry in enumerate(self.row_data):
                 new_field   = "".join([self.col_data[header1][i], self.col_data[header2][i]])
-                
+
                 self.row_data[i].append(new_field)
 
         # updates column and row data
@@ -264,11 +302,11 @@ class time_series:
         if self.discretized:
             for subset in self.subsets:
                 subset.merge_cols(header1, header2)
-        
-        return
-        
 
-    
+        return
+
+
+
     def define_time(self, time_header, fmt, start_date = False):
         """
         Converts time strings into time objects for standardized processing
@@ -283,42 +321,42 @@ class time_series:
 
         # populates self.time with time series
         self._extract_time(time_header)
-            
+
 
         # use manual start date or set to begining of first day on record
         if start_date:
             start = datetime.strptime(start_date, fmt)
-            
+
         else:
             earliest = datetime.strptime(self.time[0], fmt)
             start = datetime(earliest.year, earliest.month, earliest.day, 0,0,0,0)
 
         datestamp_list      = self.time
         self.fmt            = fmt
-        
+
         self.time_dom       = []
         self.time_dec_days  = []
-        
+
         for i,datestamp in enumerate(datestamp_list):
 
             # If error, give user information about the line on which the error occurs
             try:    t = datetime.strptime(datestamp, fmt)
             except: raise Exception("Input '{0}' in line {1} is not of format {2}".format(
                     datestamp, i+2 , fmt))
-                          
+
             self.time_dom.append(t)
-            
+
             delta = t - start
             delta = float(delta.total_seconds()) / 86400
             self.time_dec_days.append(float(delta))
-        
+
         if self.discretized:
             for subset in self.subsets:
                 subset.define_time(time_header, fmt)
 
         return
 
-    
+
     def discretize(self, subset_units):
         """
         splits the time series into individual time chunks
@@ -328,27 +366,31 @@ class time_series:
         in a dataset for a variety of purposes including data
         sanitation, periodic curve fitting, etc.
 
-        supported discretizations are as follows: 
+        supported discretizations are as follows:
         "year", "month", "day", "hour", "minute", and "second".
         """
 
         if self.discretized:
+
             for subset in self.subsets:
                 subset.discretize(subset_units)
 
-        else:   
+        else:
+
+            print("Discretizing data by {0}".format(subset_units))
+
             if self.time_dom == False:
                 raise Exception("must call 'define_time' method before discretization!")
 
             self.discretized = True
-            
+
             # set up list by which to discretize
             disc_list   = [getattr(x, subset_units) for x in self.time_dom]
 
             # set up the first subset time_series object
             counter     = 0
             new_subset  = time_series(units = subset_units)
-            
+
             new_subset._get_atts_from(self)
 
             # anywhere the subset_unit has changed make new subset time_series
@@ -367,21 +409,19 @@ class time_series:
                     # create new empty subset
                     counter += 1
                     new_subset  = time_series("temp_name", subset_units)
-                    
+
                     new_subset._get_atts_from(self)
 
             # handles the last group in the data
             new_subset.add_row(self.row_data[-1])
             new_subset.define_time(self.time_header, self.fmt)
             new_subset._name_as_subset()
-            
+
             self.subsets.append(new_subset)
 
             # rebuild the time domain
             self.define_time(self.time_header, self.fmt)
-            
-            print("Finished discretizing data by {0}".format(subset_units))
-        
+
         return
 
     def _name_as_subset(self):
@@ -393,30 +433,30 @@ class time_series:
 
         # uses the time_series object units attribute and
         # first entry in its dataset to descriptively name itself
-        
+
         subset_units = self.units
         datetime_obj = self.time_dom[0]
 
         if subset_units == "year":
             self.name = datetime_obj.strftime("%Y")
-            
+
         if subset_units == "month":
             self.name = datetime_obj.strftime("%Y-%b")
-        
+
         if subset_units == "day":
             self.name = datetime_obj.strftime("%Y-%m-%d")
-        
+
         if subset_units == "hour":
             self.name = datetime_obj.strftime("%Y-%m-%d-%H")
-        
+
         if subset_units == "minute":
             self.name = datetime_obj.strftime("%Y-%m-%d-%H:%M")
-        
+
         if subset_units == "second":
             self.name = datetime_obj.strftime("%Y-%m-%d-%H:%M:%S")
-            
+
         return
-    
+
 
     def column_stats(self, col_header):
         """
@@ -426,7 +466,7 @@ class time_series:
         """
 
         #print("Stats for time_series '{0}'. Column '{1}'".format(self.name,col_header))
-        
+
         # pull column data and find some stats
         import numpy as np
 
@@ -455,18 +495,18 @@ class time_series:
 
         if self.discretized:
             for subset in self.subsets:
-                subset.column_stats(col_header)   
+                subset.column_stats(col_header)
         return
 
 
     def normalize(col):
         """ used to normalize specific columns in the time series"""
-        
+
         raise Exception("This 'normalize' function isnt finished!")
-    
+
         return
 
-    
+
     def group_bins(self, fmt_units):
         """
         sorts the time series into time chunks by common bin_unit
@@ -490,7 +530,7 @@ class time_series:
             subset_units    = fmt_units.replace('%','')
             subset_name     = "{0}_{1}".format(subset_units, i)
             new_subset      = time_series(subset_name, subset_units)
-            
+
             new_subset._get_atts_from(self)
 
             subset_rows = [j for j,e in enumerate(grouping) if e == i]
@@ -499,7 +539,7 @@ class time_series:
 
             self.subsets.append(new_subset)
         return
-            
+
 
     def add_mono_time(self):
         """Adds a monotonically increasing time column with units of decimal days"""
@@ -519,51 +559,69 @@ class time_series:
 
 
     def interogate(self):
+        """ prints a heads up stats table of all subsets in this time_series """
+
+        if self.disc_level == 0:
+            print("")
+            print("="*89)
+            print("time_series name \t\t\t\t len \t start \t\t\t\t\t end")
+            print("="*89)
 
         if self.discretized:
 
             # use leading and trailing spaces for visualizing discretization depth
-            l = self.disc_level * "   "
-            t = (3 - self.disc_level) * "   "
-            print("{0}name: {1} {2} \t len: {3} \t start: {4} \t end: {5}".format(
-                l,
-                self.name,
-                t,
-                len(self.time),
+            padded_name = self.disc_level * "   " + self.name
+            print("{0} \t {1} \t {2} \t {3}".format(
+                padded_name.ljust(28, " "),
+                str(len(self.time)).ljust(5," "),
                 self.time_dom[0],
                 self.time_dom[-1]))
-            
+
             for subset in self.subsets:
                 subset.interogate()
 
         else:
             # use leading and trailing spaces for visualizing discretization depth
-            l = self.disc_level * "   "
-            t = (3 - self.disc_level) * "   "
-            print("{0}name: {1} {2} \t len: {3} \t start: {4} \t end: {5}".format(
-                l,
-                self.name,
-                t,
-                len(self.time),
+            padded_name = self.disc_level * "   " + self.name
+            print("{0} \t {1} \t {2} \t {3}".format(
+                padded_name.ljust(28, " "),
+                str(len(self.time)).ljust(5," "),
                 self.time_dom[0],
                 self.time_dom[-1]))
         return
 
 
-# testing esting
+# testing
 if __name__ == "__main__":
 
     # testing csv manipulations
-    ts = time_series('master')
+    ts = time_series('Master_TS')
     ts.from_csv("separate_date_time.csv")
-    ts.merge_cols("date","time")
-    ts.define_time("date_time","%Y%m%d%H%M%S","20000101000000")
+    ts.merge_cols("date", "time")
+    ts.define_time("date_time", "%Y%m%d%H%M%S", "20000101000000")
     ts.add_mono_time()
-    #ts.discretize('day')
+
+    print("time series with no subsets")
+    print "ts[0]         :", ts[0]
+    print "ts[0][0]      :", ts[0][0]
+
+
+    ts.discretize('day')
+    print("time series with one subset layer")
+    print "ts[0]         :", ts[0]
+    print "ts[0][0]      :", ts[0][0]
+    print "ts[0][0][0]   :", ts[0][0][0]
+
+
+    ts.discretize('hour')
+    print("time series with two subset layers")
+    print "ts[0]         :", ts[0]
+    print "ts[0][0]      :", ts[0][0]
+    print "ts[0][0][0]   :", ts[0][0][0]
+    print "ts[0][0][0][0]:", ts[0][0][0][0]
+
     ts.interogate()
 
-    print ts[0]
-    
 
 
 
@@ -589,5 +647,3 @@ if __name__ == "__main__":
 
 
 
-
-    
