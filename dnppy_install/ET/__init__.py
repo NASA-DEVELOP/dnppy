06a13620afcde0dcdc70ed99a4649ae6472fa887
Jeff <jeff.ely.08@gmail.com>
2015-04-16 13:15:05 -0400

    renamed metric to ET module.

---
+++ dnppy_install/ET/__init__.py
@@ -0,0 +1,9 @@
+
+"""
+The EvapoTranspiration (ET) module has been added to contain
+all ET related code, starting with the METRIC model.
+
+It is not as scalable as it could be, but it met the requirements
+of the first partner handoff involving ET calculations.
+"""
+
