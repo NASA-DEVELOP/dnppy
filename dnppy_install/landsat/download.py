23e04d00fc37769849bf8a23edb2567df2666662
Daniel Jensen <danieljohnjensen@gmail.com>
2015-04-23 16:35:57 -0700

    landsat download
    Completed landsat sample download function. Started a Landsat 8 series
    download function in landsat.download to enable downloading a time
    series of OLI data.

--- dnppy_install/landsat/download.py
+++ dnppy_install/landsat/download.py
@@ -1,52 +1,59 @@
+import urllib
+import webbrowser
 from dnppy import download
 from dnppy import core
 
-__all__=['dl_test_data']          # planned development
+__all__=['test_data'             # completed
+         'landsat_8_series']        # planned development
 
-def dl_test_data(landsat_list, outdir):
+def test_data(landsat_number):
     """
     Downloads a dataset for testing from Landsat 4, 5, 7, or 8
 
-    *Note that for Landsat 4, 5, and 7 you must be signed into earthexplorer.usgs.gov
-    *The Landsat 8 dataset is hosted on Amazon Web Services and is freely accessible
+    *Note that you must be signed into earthexplorer.usgs.gov
     
     Inputs:
-      landsat   A list of [4, 5, 7, and/or 8] - the desired Landsat satellites to sample data from
-      outdir    Output directory to save the downloaded dataset
+      landsat_number   4, 5, 7, or 8 - the desired Landsat satellites to sample data from
+
     """
-
-    core.enforce.enf_list(landsat_list)
-    urls_dl = []
     
-    for landsat in landsat_list:
-        if landsat == 8:
-            url = "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/041/036/LC80410362014280LGN00/"
-            name = "LC80410362014280LGN00"
-            i = 1
-            while i <= 11:
-                filename = url + name + "_B{0}.TIF".format(i)
-                urls_dl.append(filename)
-                i = i + 1
-            BQA = url + name + "_BQA.TIF"
-            urls_dl.append(BQA)
-            meta = url + name + "_MTL.txt"
-            urls_dl.append(meta)
-            outfolder = "{0}\{1}".format(outdir, name)
-        elif landsat == 7:
-            url = "http://earthexplorer.usgs.gov/download/3372/LE70410362003114EDC00/STANDARD"
-            name = "LE70410362003114EDC00"
-            urls_dl.append(url)
-        elif landsat == 5:
-            url = "http://earthexplorer.usgs.gov/download/3119/LT50410362011208PAC01/STANDARD"
-            name = "LT50410362011208PAC01"
-            urls_dl.append(url)
-        elif landsat == 4:
-            url = "http://earthexplorer.usgs.gov/download/3119/LT40410361990014XXX01/STANDARD"
-            name  = "LT40410361990014XXX01"
-            urls_dl.append(url)
-        else:
-            print "Please enter 4, 5, 7, and/or 8 in list format"
-
-    download.urls(urls_dl, ["TIF","txt","gz"], outdir)
+    landsat = int(landsat_number)
+    
+    if landsat == 8:
+        url = "http://earthexplorer.usgs.gov/download/4923/LC80410362014232LGN00/STANDARD"
+        webbrowser.open(url)
+    elif landsat == 7:
+        url = "http://earthexplorer.usgs.gov/download/3372/LE70410362003114EDC00/STANDARD"
+        webbrowser.open(url)
+    elif landsat == 5:
+        url = "http://earthexplorer.usgs.gov/download/3119/LT50410362011208PAC01/STANDARD"
+        webbrowser.open(url)
+    elif landsat == 4:
+        url = "http://earthexplorer.usgs.gov/download/3119/LT40410361990014XXX01/STANDARD"
+        webbrowser.open(url)
+    else:
+        print "Please enter 4, 5, 7, or 8"
     
     return
+
+def landsat_8_series(path, row, start_day, end_day, outdir):
+    """
+    An idea for making use of the amazon web service hosted Landsat 8 OLI data.
+    This will allow a user to identify the path/row and range of days to automatically download
+    a time series of OLI data. The code below downloads each band tiff on the given url.
+    """
+
+    urls_dl = []
+    url = "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/041/036/LC80410362014280LGN00/"
+    name = "LC80410362014280LGN00"
+    i = 1
+    while i <= 11:
+        filename = url + name + "_B{0}.TIF".format(i)
+        urls_dl.append(filename)
+        i = i + 1
+    BQA = url + name + "_BQA.TIF"
+    urls_dl.append(BQA)
+    meta = url + name + "_MTL.txt"
+    urls_dl.append(meta)
+    outfolder = "{0}\{1}".format(outdir, name)
+    download.urls(urls_dl, ["TIF","txt"], outdir)
