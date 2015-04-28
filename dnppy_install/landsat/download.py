5de8da1122ae1aaee789400bed48e3953cde2344
Daniel Jensen <danieljohnjensen@gmail.com>
2015-04-27 17:01:33 -0700

    Landsat 8 Download, UAVSAR
    Finished the Landsat 8 individual scene download function, which uses
    AWS. This function will be incorporated into the series download
    function. Also started the radar module and UAVSAR header creation
    function.

--- dnppy_install/landsat/download.py
+++ dnppy_install/landsat/download.py
@@ -36,16 +36,53 @@ def test_data(landsat_number):
     
     return
 
-def landsat_8_series(path, row, start_day, end_day, outdir):
+def landsat_8_scene(path, row, year, day, outdir):
     """
-    An idea for making use of the amazon web service hosted Landsat 8 OLI data.
-    This will allow a user to identify the path/row and range of days to automatically download
-    a time series of OLI data. The code below downloads each band tiff on the given url.
+    This function makes use of the amazon web service hosted Landsat 8 OLI data.
+    This allows the user to identify the path/row and year/day to automatically download
+    a scene of OLI data. The code below downloads each band tiff in the dataset.
+
+    *Note: for best results, enter number inputs in quotemarks as strings.
+    Some identifier integers may be read by Python wrong (e.g. 010 is read as 8 in binary)
+
+    *You can use this tool to find the Path/Row values based on Latitude/Longitude coordinates.
+    https://landsat.usgs.gov/tools_latlong.php
+
+    *You can refer to this table to find the Julian Day value for your desired dataset
+    http://amsu.cira.colostate.edu/julian.html
+    
+    Inputs:
+        path: the Landsat path number for the desired dataset
+        row: the Landsat row number for the desired dataset
+        year: the four digit year number the desired dataset was created
+        day: the three digit Julian Day number the desired dataset was created (e.g. January 1 = 001)
+        outdir: the output location for the downloaded Landsat 8 dateset
+        
     """
 
     urls_dl = []
-    url = "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/041/036/LC80410362014280LGN00/"
-    name = "LC80410362014280LGN00"
+
+    path_str = str(path)
+    row_str = str(row)
+    year_str = str(year)
+    day_str = str(day)
+
+    if len(path_str) == 2:
+        path_str = "0" + path_str
+    elif len(path_str) == 1:
+        path_str = "00" + path_str
+    if len(row_str) == 2:
+        row_str = "0" + row_str
+    elif len(row_str) == 1:
+        row_str = "00" + row_str
+    if len(day_str) == 2:
+        day_str = "0" + day_str
+    elif len(day_str) == 1:
+        day_str = "00" + day_str
+
+    name = "LC8{0}{1}{2}{3}LGN00".format(path_str, row_str, year_str, day_str)
+    url = "https://s3-us-west-2.amazonaws.com/landsat-pds/L8/{0}/{1}/{2}/".format(path_str, row_str, name)
+
     i = 1
     while i <= 11:
         filename = url + name + "_B{0}.TIF".format(i)
@@ -56,4 +93,11 @@ def landsat_8_series(path, row, start_day, end_day, outdir):
     meta = url + name + "_MTL.txt"
     urls_dl.append(meta)
     outfolder = "{0}\{1}".format(outdir, name)
-    download.urls(urls_dl, ["TIF","txt"], outdir)
+    download.urls(urls_dl, outfolder, ["TIF","txt"])
+
+def landsat_8_series(path, row, start_year, start_day, end_year, end_day, outdir):
+    """
+    An idea for making use of the amazon web service hosted Landsat 8 OLI data.
+    This will allow a user to identify the path/row and year/day to automatically download
+    a time series of OLI data. The code below downloads each band tiff on the given url.
+    """
---
+++ dnppy_install/radar/uavsar.py
@@ -0,0 +1,10 @@
+__all__= ['uavsar_header']        # planned development
+
+def uavsar_header(stuff):
+    """
+    Builds a header file for the input UAVSAR .grd file,
+    allowing the data to be read as a raster dataset.
+
+    Inputs:
+        stuff   description of stuff
+    """
