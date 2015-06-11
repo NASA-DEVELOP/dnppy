8b499799504ef342af3dac7023a47bcc7bded69b
Daniel Jensen <danieljohnjensen@gmail.com>
2015-06-10 17:14:35 -0700

    Added extract_HDF5_layers
    Function works - but is bare bones right now. Still needs editing to
    handle different inputs and naming conventions, or be combined with HDF4
    function.

--- dnppy_install/convert/extract_HDF_layers.py
+++ dnppy_install/convert/extract_HDF_layers.py
@@ -2,12 +2,13 @@
 # standard imports
 import os
 from dnppy import core
+import gdal
 import arcpy
 if arcpy.CheckExtension('Spatial')=='Available':
     arcpy.CheckOutExtension('Spatial')
     arcpy.env.overwriteOutput = True
 
-def extract_HDF_layers(filelist, layerlist, layernames = None, outdir = None):
+def extract_HDF4_layers(filelist, layerlist, layernames = None, outdir = None):
 
     """
      Function extracts tifs from HDFs.
@@ -81,6 +82,45 @@ def extract_HDF_layers(filelist, layerlist, layernames = None, outdir = None):
 
 
 
+def extract_HDF5_layers(filelist, layerlist, lowerleft_lat, lowerleft_lon, cellsize, outdir):
+    """
+     Function extracts tifs from HDFs.
+     Use "Extract_MODIS_HDF" in the modis module for better
+     handling of MODIS data with sinusoidal projections.
+
+     inputs:
+       filelist    list of '.hdf' files from which data should be extracted
+       layerlist   list of layer numbers to pull out as individual tifs should be integers
+                   such as [0,4] for the 0th and 4th layer respectively.
+       layernames  list of layer names to put more descriptive names to each layer
+       outdir      directory to which tif files should be saved
+                   if outdir is left as 'False', files are saved in the same directory as
+                   the input file was found.
+    """
+
+    h5list = []
+    corner = arcpy.Point(lowerleft_lon, lowerleft_lat)
+
+    for file in filelist:
+        h5 = gdal.Open(file)
+        h5_sds = h5.GetSubDatasets()
+##        if layernames == None:
+        for layer in layerlist:
+            h5_sd = gdal.Open(h5_sds[layer][0])
+            h5_arr = h5_sd.ReadAsArray()
+            h5list.append(h5_arr)
+##        else:
+##            for layer in layernames:
+##                h5_sd = gdal.Open(h5_sds[layer][0])
+##                h5_arr = h5_sd.ReadAsArray()
+##                h5list.append(h5_arr)
+
+            x = 1
+            for h5 in h5list:
+                h5_rast = arcpy.NumPyArrayToRaster(h5, corner, cellsize)
+                h5_rast.save("{0}\\{1}_sd{2}.tif".format(outdir, "test", layer))
+
+
 # testing area
 if __name__ == "__main__":
 
@@ -88,4 +128,4 @@ if __name__ == "__main__":
     filelist = r"C:\Users\Jeff\Desktop\GPM_testing\2015-04-01"
     layerlist = [0,1,2,3,4,5,6,7]
 
-    extract_HDF_layers(filelist, layerlist)
+    extract_HDF4_layers(filelist, layerlist)
