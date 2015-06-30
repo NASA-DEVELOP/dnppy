e59043380d7da516d0ecc96801aafef437daa67c
Jeff <jeff.ely.08@gmail.com>
2015-06-30 15:11:21 -0400

    started nongrid_data class for gridding swath data.

--- dnppy_install/convert/nongrid_data_class.py
+++ dnppy_install/convert/nongrid_data_class.py
@@ -4,7 +4,7 @@ __all__ = ["nongrid_data"]
 from LLtoUTM import *
 import numpy
 import math
-import scipy
+from scipy import interpolate
 
 class nongrid_data():
     """
@@ -62,35 +62,42 @@ class nongrid_data():
         else:
             self.hemisphere = "N"
 
+        # converts lat lon points to utmx and utmy points
+        self.utmx, self.utmy = LLtoUTM(self.lat, self.lon, self.utm_zone, self.hemisphere)
+
         # find min and maximum utm coordinates
-        self.min_utmx, self.min_utmy = LLtoUTM(self.min_lat, self.min_lon,
-                                               self.utm_zone, self.hemisphere)
-        self.max_utmx, self.max_utmy = LLtoUTM(self.max_lat, self.max_lon,
-                                               self.utm_zone, self.hemisphere)
-
-        # estimate the spatial resolution of the data
-        W = self.distance(self.min_lat, self.min_lon, self.max_lat, self.min_lon)
-        H = self.distance(self.min_lat, self.min_lon, self.min_lat, self.max_lon)
-
-        self.est_resolution = ((W * H) / len(self.data)) ** 0.5
+        self.min_utmx = numpy.min(self.utmx)
+        self.min_utmy = numpy.min(self.utmy)
+        self.max_utmx = numpy.max(self.utmx)
+        self.max_utmy = numpy.max(self.utmy)
 
         # print a summary
         print("location: UTM zone {0}{1}".format(self.utm_zone, self.hemisphere))
-        print("resolution: {0} m".format(self.est_resolution))
-        return
-
+        print("LL UTM coordinates: {0} , {1}".format(self.min_utmx, self.min_utmy))
+        print("UR UTM coordinates: {0} , {1}".format(self.max_utmx, self.max_utmy))
 
 
     @staticmethod
-    def distance(lat0, lon0, lat1, lon1):
+    def distance_x_y(x0, y0, x1, y1):
+        """
+        computes the approx distance between two utmx, utmy locations
+        """
+
+        dist = (((x0 - x1) **2) + ((y0 - y1)**2)) ** 0.5
+        return dist
+
+
+    @staticmethod
+    def distance_lat_lon(lat0, lon0, lat1, lon1):
         """
         computes the distance between two lat/lon coordinates in meters
-        using the Haversine formula. Radians are the unit!
+        using the Haversine formula.
         """
 
         # radius of earth in meters
         R = 6371000
 
+        # convert degree values to radians.
         lat0 = math.radians(lat0)
         lon0 = math.radians(lon0)
         lat1 = math.radians(lat1)
@@ -108,24 +115,54 @@ class nongrid_data():
         return dist
 
 
+    def _sample_by_location(self, utmx0, utmy0, resolution):
+        """ performs grid interpolation for a single location """
+
+        # find points in the dataset that are near the input x,y
+
+        #
+
+
+
+        subutmx, subutmy, subdata = [(x, y, z) if self.distance_x_y(utmx0, utmy0, x, y) < (2 * resolution) else (None, None, None) \
+                                   for x,y,z in zip(self.utmx, self.utmy, self.data)]
+
+        # create single point meshgrid and interpolate its value from nearby points
+        points = (subutmx, subutmy)
+        meshgrid = numpy.mgrid(utmx0, utmy0)
+        location_data_value = interpolate.griddata(points, subdata, meshgrid, method = "cubic")
+
+        return location_data_value
+
 
     def sample_by_grid(self, resolution):
         """
         Grids a dataset to the desired resolution in a UTM projection. input
-        resolution is the length of one pixel on a side in meters.
+        resolution is the length of one square pixel on a side in meters.
         """
 
-        # build a meshgrid
-        xgrid, ygrid = numpy.meshgrid[self.min_utmx:self.max_utmx:complex(0, resolution),
-                                      self.min_utmy:self.max_utmy:complex(0, resolution)]
+        xrange = numpy.arange(self.min_utmx, self.max_utmx, resolution)
+        yrange = numpy.arange(self.min_utmy, self.max_utmy, resolution)
 
+        outgrid = numpy.zeros((len(xrange), len(yrange)))
+        print outgrid.shape
+        print("griding dataset, this may take a while")
+        for xi, x in enumerate(xrange):
+            for yi, y in enumerate(yrange):
+                outgrid[x,y] = self._sample_by_location(x, y, resolution)
 
-
-        return
+        return outgrid
 
 
 # testing area
 if __name__ == "__main__":
-    pass
+    from _extract_HDF_layer_data import *
 
-    numpy.mgrid()
+    rasterpath = r"C:\Users\jwely\Desktop\troubleshooting\HDF_tests\GDNBO-SVDNB_npp_d20150626_t0132557_e0138361_b18964_c20150626174428799822_noaa_ops.h5"
+    layer_data = _extract_HDF_layer_data(rasterpath, [2, 4, 19])
+
+    ngd = nongrid_data(layer_data[2].ReadAsArray(),
+                        layer_data[4].ReadAsArray(),
+                        layer_data[19].ReadAsArray())
+
+    ngd.sample_by_grid(resolution = 10000)
