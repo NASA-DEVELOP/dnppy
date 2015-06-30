45586510b00f0ae66b52da08852c216493ce5c02
Jeff <jeff.ely.08@gmail.com>
2015-06-30 15:12:21 -0400

    started extract_SMOS_netCDF

---
+++ dnppy_install/convert/extract_SMOS_NetCDF.py
@@ -0,0 +1,23 @@
+__author__ = 'jwely'
+
+def extract_SMOS_NetCDF(netcdf_list, layer, outdir):
+    """
+    extracts SMOS data from its native NetCDF format.
+    :param netcdf_list:
+    :param layer:
+    :param outdir:
+    :return:
+    """
+
+
+
+
+
+    return
+
+
+if __name__ == "__main__":
+
+    filepath = r"C:\Users\jwely\Desktop\troubleshooting\SMOS\_NRTSM003D025A_ALL.nc"
+    outdir = r"C:\Users\jwely\Desktop\troubleshooting\SMOS"
+    extract_SMOS_NetCDF(filepath, [0], outdir)
