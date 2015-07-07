db279ab7e24e975437c2c0bf9080cd1c22c3f3bf
Jeff <jeff.ely.08@gmail.com>
2015-07-07 18:55:04 -0400

    added MPE to download __init__
    closes #40

--- dnppy_install/download/__init__.py
+++ dnppy_install/download/__init__.py
@@ -16,12 +16,14 @@ from download_filelist import *
 from download_url import *
 from download_urls import *
 
-from fetch_SRTM import *
 from fetch_GPM_IMERG import *
-from fetch_landsatWELD import *
-from fetch_TRMM import *
-from fetch_MODIS import *
 from fetch_Landsat8 import *
+from fetch_landsatWELD import *
+from fetch_MODIS import *
+from fetch_MPE import *
+from fetch_SRTM import *
+from fetch_TRMM import *
+
 
 from list_http_e4ftl01 import *
 from list_ftp import *
