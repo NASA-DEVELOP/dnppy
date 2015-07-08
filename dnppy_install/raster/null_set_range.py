1d61d18b431a8c0e9e455b727f0db37fabb47b7d
Jeff <jeff.ely.08@gmail.com>
2015-07-08 15:02:29 -0400

    updated null_set_range with exception clause

--- dnppy_install/raster/null_set_range.py
+++ dnppy_install/raster/null_set_range.py
@@ -6,7 +6,6 @@ from to_numpy import to_numpy
 from from_numpy import from_numpy
 
 import arcpy
-import os
 
 def null_set_range(rastlist, high_thresh = None, low_thresh = None, NoData_Value = None):
     """
@@ -27,19 +26,24 @@ def null_set_range(rastlist, high_thresh = None, low_thresh = None, NoData_Value
 
     # iterate through each file in the filelist and set nodata values
     for rastname in rastlist:
+
         #load raster as numpy array and save spatial referencing.
         rast, meta = to_numpy(rastname)
 
-        if not NoData_Value == None:
+        if not NoData_Value is None:
             NoData_Value = meta.NoData_Value
 
-        if not high_thresh == None:
+        if not high_thresh is None:
             rast[rast >= high_thresh] = NoData_Value
-        if not low_thresh == None:
+
+        if not low_thresh is None:
             rast[rast <= low_thresh] = NoData_Value
 
         from_numpy(rast, meta, rastname)
-        arcpy.SetRasterProperties_management(rastname, data_type="#",statistics="#",
+        try:
+            arcpy.SetRasterProperties_management(rastname, data_type = "#", statistics = "#",
                     stats_file = "#", nodata = "1 " + str(NoData_Value))
+        except RuntimeError:
+            print("failed to set nodata in {0}".format(rastname))
 
     return
