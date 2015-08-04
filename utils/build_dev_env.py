58d6ebc69bdb8a492683dc93237a525ee9d5633b
Jeff <jeff.ely.08@gmail.com>
2015-08-04 11:10:46 -0400

    added path instructions

--- utils/build_dev_env.py
+++ utils/build_dev_env.py
@@ -5,12 +5,14 @@ def build_dev_env():
     builds the development environment used to create autodocs through
     sphinx.
     """
-    import pip
-    sphinx = pip.main(["install","sphinx"])
-    graphviz = pip.main(["install","graphviz"])
 
-    if sphinx == 0 and graphviz == 0:
-        print("Modules ready!, ensure sphinx-build is added to PATH and continue!")
+    import pip
+    got_sphinx = pip.main(["install","sphinx"])
+    got_graphviz = pip.main(["install","graphviz"])
+
+    if got_sphinx == 0 and got_graphviz == 0:
+        path = pip.__file__.replace("lib\\site-packages\\pip\\__init__.pyc", "Scripts")
+        print("Modules ready!, ensure '{0}' is added to your PATH variable to continue!".format(path))
 
 
 def main():
