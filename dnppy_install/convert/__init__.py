6f241fa764068e71cec35c35a362321cad6dd35e
Jeff Ely <jeff.ely.08@gmail.com>
2015-08-08 14:37:29 -0400

    update convert module description

--- dnppy_install/convert/__init__.py
+++ dnppy_install/convert/__init__.py
@@ -1,5 +1,8 @@
 """
-dnppy convert contains functions for misc conversion between formats and types. 
+dnppy convert contains functions for conversion between filetypes. Usually between
+a file type distributed by a NASA Distributed Active Archive Center (DAAC) such as
+NetCDF or HDF5 to geotiff. Due to differences in metadata standards, many of these
+functions only operate successfully data from a specific source.
 """
 
 __author__ = ["Jeffry Ely, jeff.ely.08@gmail.com"]
