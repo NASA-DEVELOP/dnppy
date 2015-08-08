0e20ff713745945618401910e77661780ac302ef
Jeff Ely <jeff.ely.08@gmail.com>
2015-08-07 23:15:19 -0400

    trying other pygments, they dont seem to work

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,6 +15,7 @@
 import sys
 import os
 import shlex
+from pygments import styles
 
 # If extensions (or modules to document with autodoc) are in another directory,
 # add these directories to sys.path here. If the directory is relative to the
@@ -102,7 +103,7 @@ exclude_patterns = []
 #show_authors = False
 
 # The name of the Pygments (syntax highlighting) style to use.
-pygments_style = 'sphinx'
+pygments_style = 'default'
 
 # A list of ignored prefixes for module index sorting.
 #modindex_common_prefix = []
