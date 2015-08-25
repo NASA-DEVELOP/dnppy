a8db7909f36042b5d25443894f3923dfead0d7ce
Jeff <jeff.ely.08@gmail.com>
2015-08-25 11:34:05 -0400

    download_url default wont overwrite
    added force_overwrite argument to override new feature where function
    will not download a file if its destination name already exists on disk.

--- dnppy/download/download_url.py
+++ dnppy/download/download_url.py
@@ -7,17 +7,24 @@ import os
 __all__ = ["download_url"]
 
 
-def download_url(url, outname, username = None, password = None):
+def download_url(url, outname, username = None, password = None, force_overwrite = False):
     """
     Download a single file. input source url and output filename
 
-    :param url:         a string url to file for download
-    :param outname:     filepath to write location of file
-    :param username:    use if url leads to file on ftp server with username
-    :param password:    use if url leads to file on ftp server with password
+    :param url:             a string url to file for download
+    :param outname:         filepath to write location of file
+    :param username:        use if url leads to file on ftp server with username
+    :param password:        use if url leads to file on ftp server with password
+    :param force_overwrite: by default, this function will not overwrite existing files
+                            on drive. This is to avoid accidental overwrite of critical
+                            files as well as repeated download of the same files.
 
     :return outname:    returns filepath to locally created file after download
     """
+
+    # if output file already exists, do not download a file there.
+    if os.path.isfile(os.path.abspath(outname)) and not force_overwrite:
+        return outname
 
     head, tail = os.path.split(outname)
     if not os.path.exists(head) and head is not "":
