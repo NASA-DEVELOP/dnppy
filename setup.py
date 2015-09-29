17770d0d276d6673edf86f7e3d21cc2afa5f6c3a
Jeff Ely <jeff.ely.08@gmail.com>
2015-09-29 12:10:51 -0400

    version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ from distutils.core import setup
 
 setup(
     name='dnppy',
-    version='1.15.3b1',
+    version='1.15.3b2',
     packages=['dnppy',
               'dnppy.convert',
               'dnppy.core',
